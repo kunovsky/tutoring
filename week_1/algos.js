0e17e5f0b18a8b85aade586e6a85d86726096a04
Ty Kunovsky <tyson.kunovsky@slalom.com>
2016-02-21 16:02:39 -0600

    implemented first 2 algos with Tyler and Stephon

--- week_1/algos.js
+++ week_1/algos.js
@@ -2,18 +2,45 @@
 // for loops, while loops, functions, if else statements, data structures (arrays, objects)
 // http://eloquentjavascript.net/Eloquent_JavaScript.pdf
 
+
 function greeter(name) {
-  console.log("Hi );
+  console.log("Hi " + name);
 }
 
-greeter("Ty")
+ greeter("Ty")
 
 function reverseString(str) {
+  //  declare variable using "var keyword"
+  // using a "for loop" we loop over the input string
+  // next we index into string "[]"
+  // get the letter and add to the result
+  // we must return our variable
+  var results = "";
+  for(var i = str.length-1; i >= 0; i --) {
+    results  = results + str[i];
+  }
+  return results;
 }
 
 function reverseWordsInString(str) {
 
+  // split these words on an empty space
+  // for each word go through and reverse the word
+  // return the result
+  var words = str.split(" ");
+var results = "";
+  for(var i = 0; i< words.length; i ++) {
+    results = results + reverseString(words[i]);
+results += " "
+  }
+  return results;
+
+
 }
+ var myString = "reverse the words in this string";
+
+ console.log(reverseWordsInString(myString));
+
 
 function mostCommonCharacter(str) {
 
@@ -40,5 +67,5 @@ function rand7(){
 }
 
 function findIndex(values, target) {
-  
+
 }
