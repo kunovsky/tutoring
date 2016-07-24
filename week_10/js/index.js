b47689e7180af4ba2a40aa4685c4a291e89abb30
Tyson Kunovsky <tkunovsky@gmail.com>
2016-07-24 18:26:18 -0500

    wip for our javascript game

--- week_10/js/index.js
+++ week_10/js/index.js
@@ -1,4 +1,6 @@
-var COLORS = ["color1","color2","color3","color4"]
+var COLORS = ["color1","color2","color3","color4"];
+var squaresToSwap = [];
+
 // four rows
 // each row has a randomized set of colors
 // timed game
@@ -9,7 +11,7 @@ function startGame() {
   var colors = []
   var squares =  $(".square");
 
-  for (var i = 0; i< 4; i++){
+  for (var i = 0; i < 4; i++){
     COLORS.forEach(function(color) {
       colors.push(color);
     })
@@ -25,3 +27,43 @@ function startGame() {
 }
 
 startGame()
+
+$(".square").click(function(event){
+  var id = $(event.target).attr('id');
+  squaresToSwap.push(id);
+  if (squaresToSwap.length === 2){
+    var squareOne = $("#" + squaresToSwap[0]);
+    var squareTwo = $("#" + squaresToSwap[1]);
+    var squareOneClasses = squareOne.attr("class");
+    var squareTwoClasses = squareTwo.attr("class");
+
+    $(squareOne).removeClass(squareOneClasses);
+    $(squareTwo).removeClass(squareTwoClasses);
+
+    $(squareOne).addClass(squareTwoClasses);
+    $(squareTwo).addClass(squareOneClasses);
+
+    squaresToSwap = [];
+
+    checkGameForWinner();
+  }
+});
+
+function checkGameForWinner() {
+  var won = true;
+  var rows = $(".row");
+
+  for (var i = 0; i< rows.length; i++) {
+    var row = rows[i];
+    var classes = $(row.children[0]).attr('class')
+    for (var j = 0; j < row.children.length; j++) {
+      if ($(row.children[j]).attr('class') !== classes) {
+        won = false;
+      }
+    }
+  }
+
+  if (won) {
+    alert("you win!")
+  }
+}
